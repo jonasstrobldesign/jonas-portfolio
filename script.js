82e261a1e0b3c75631a01003172e4229c7931cc9
Jonas <jonas@Jonass-MBP.fritz.box>
2025-12-24 12:23:04 +0100

    Update site layout

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-/* -------------------------------------- */
-/* MOBILE MENU */
-/* -------------------------------------- */
+// Intro Screen
 
-function togglemenu(event) {
-    event.stopPropagation();
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.intro-logo');
 
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx+1)*150)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span,idx)=>{
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx+1)*50)
+            })
+        },2000)
+
+        setTimeout(()=>{
+            intro.style.top='-100vh';
+        },2300)
+    })
+})
+
+// Scroll to top upon reload
+
+window.onload = function() {
+    window.scrollTo(0, 0);
+};
+
+// Hamburger navbar
+
+function togglemenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 
+    // If the menu is open, add a click event listener to the document
     if (menu.classList.contains("open")) {
         document.addEventListener("click", closeMenuOnClickOutside);
     } else {
@@ -22,31 +54,42 @@ function closeMenuOnClickOutside(event) {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
 
+    // Check if the click is outside the menu and the hamburger icon
     if (!menu.contains(event.target) && !icon.contains(event.target)) {
-        closeMenu();
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+        
+        // Remove the event listener after closing the menu
+        document.removeEventListener("click", closeMenuOnClickOutside);
     }
 }
 
-function closeMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
 
-    menu.classList.remove("open");
-    icon.classList.remove("open");
+// Into fade-blur transition
 
-    document.removeEventListener("click", closeMenuOnClickOutside);
-}
-
-/* -------------------------------------- */
-/* SCROLL REVEAL */
-/* -------------------------------------- */
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } 
     });
 });
 
-document.querySelectorAll(".reveal").forEach((el) => observer.observe(el));
+const hiddenElements = document.querySelectorAll('.section__text');
+hiddenElements.forEach((el)=>observer.observe(el));
+
+// Navbar turns black on scroll
+
+// function changeBG() {
+//     var navbar = document.getElementById('desktop-nav');
+//     var scrollValue = window.scrollY;
+//     if(scrollValue < 25) {
+//         navbar.classList.remove('bgcolor')
+//     } else {
+//         navbar.classList.add('bgcolor')
+//     }
+// }
+
+// window.addEventListener('scroll', changeBG)
+
